a4f2cb94b3645539145ef03cd93c35504e289843
Aman Roy <yadavaman7632@gmail.com>
2024-03-03 01:00:57 +0530

    added projects section_

--- src/Components/project/Project.jsx
+++ src/Components/project/Project.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import ecom from './../images/ecom.png'
+import weatherImg from './../images/weather.png'
 import './project.css'
 function Project() {
     const projects = [
         {
           id: 1,
           title: "E-Commerce",
-          description: "Description of Project 1",
-          image: "https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA"
+          description: "Latest Project",
+          image: ecom
+          ,link:"https://e-commerce-caip-dm562z76a-amanroy7632s-projects.vercel.app"
         },
         {
           id: 2,
           title: "Weather App",
           description: "Description of Project 2",
-          image: "https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA"
+          image: weatherImg
+          ,link:"https://my-wather-app-ten.vercel.app/"
         },
         {
           id: 3,
           title: "Currency Convertor",
           description: "Description of Project 3",
-          image: "https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA"
+          image: ""
+          ,link:""
         },
         {
             id: 4,
             title: "Blog App",
             description: "Description of Project 3",
-            image: "https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA"
+            image: ""
+            ,link:""
           }
       ];
   return (
@@ -34,7 +41,7 @@ function Project() {
       {projects.map(project => (
         <div className="project" key={project.id}>
           {/* <img src={require(`${project.image}`).default}  alt={project.title} /> */}
-          <img src={project.image}  alt={project.title} />
+       <Link to={project.link}> <img src={project.image}  alt={project.title} /> </Link>  
           <div className="project-info">
             <h2 className='title'>{project.title}</h2>
             <p>{project.description}</p>
