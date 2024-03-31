d8f74cd717bf40ae27718584ff68de728c6b8943
Aman Roy <yadavaman7632@gmail.com>
2024-03-31 10:50:18 +0530

    added filtering features

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,26 +1,60 @@
-import './../CssFile/about.css';
-import profilePic from './../images/about_img.png';
-import { useState } from 'react';
-const string="I bring a wealth of experience and a passion for crafting robust, scalable, and innovative software. From intricate coding challenges to seamless user experiences, my commitment to excellence is evident in every line of code. Join me on a journey where technology meets precision, and together, let's build the future of digital innovation. Welcome to a world of unparalleled development expertise - welcome to my Portfolio.";
+import "./../CssFile/about.css";
+import profilePic from "./../images/about_img.png";
+import { useState } from "react";
+import debugCertificateimg from "./certificate/debug.png"
+const string =
+  "I bring a wealth of experience and a passion for crafting robust, scalable, and innovative software. From intricate coding challenges to seamless user experiences, my commitment to excellence is evident in every line of code. Join me on a journey where technology meets precision, and together, let's build the future of digital innovation. Welcome to a world of unparalleled development expertise - welcome to my Portfolio.";
 function About() {
-    const[readMore,setReadmore]=useState(false);
-    const description=readMore ? string:`${string.substring(0,200)}...`;
-    function readMoreHandler(){
-        document.querySelector(".about-img").classList.toggle("visible");
-        document.querySelector(".readMore").classList.toggle("change-color");
-        setReadmore(!readMore);
-    }
-    return (<div id="about" className='about max-md:mt-[5vh] flex justify-evenly items-center flex-nowrap w-[100vw] h-[90vh] gap-[2rem]'>
-        <div className="about-img">
-            <img src={profilePic} alt="profile pic" />
-        </div>
-        <div className="about-content">
-            <h2 className="heading font-semibold">About <span className='span-color'>Me</span></h2>
-            <h3>Hello! Welcome to my <span>Portfolio</span>! Really happy to see you here.</h3>
-            <p> {description}<span className='readMore' onClick={readMoreHandler}>{readMore?`show less`:`read more`}</span></p>
+  const [readMore, setReadmore] = useState(false);
+  const description = readMore ? string : `${string.substring(0, 200)}...`;
+  function readMoreHandler() {
+    document.querySelector(".about-img").classList.toggle("visible");
+    document.querySelector(".readMore").classList.toggle("change-color");
+    setReadmore(!readMore);
+  }
+  return (
+    <>
+    <div
+      id="about"
+      className="about max-md:mt-[5vh] flex justify-evenly items-center flex-nowrap w-[100vw] mt-[12vh] gap-[2rem] transition-all duration-200"
+    >
+      <div className="about-img w-[40%] max-sm:w-[80%]">
+        <img src={profilePic} alt="profile pic" />
+      </div>
+      <div className="about-content transition-all duration-200 w-full">
+        <h2 className="heading font-semibold">
+          About <span className="span-color">Me</span>
+        </h2>
+        <h3>
+          Hello! Welcome to my <span>Portfolio</span>! Really happy to see you
+          here.
+        </h3>
+        <p>
+          {" "}
+          {description}
+          <span className="readMore" onClick={readMoreHandler}>
+            {readMore ? `show less` : `read more`}
+          </span>
+        </p>
 
-            {/* <a href="#" className="btn">Read More</a> */}
+        {/* <a href="#" className="btn">Read More</a> */}
+      </div>
+    </div>
+    <div className="projects-container">
+    <h1 className='heading mb-10 font-semibold'><span className='span-color'>My </span>Acheivemnts</h1>
+    <div className="projects">
+      
+        <div className="project dark:bg-[#323946]">
+          {/* <img src={require(`${project.image}`).default}  alt={project.title} /> */}
+         <img src={debugCertificateimg}  alt="debug certificate image" /> 
+          <div className="project-info">
+            <h2 className='title'>I got 1 <sup>st</sup> prize in debugging contest which is organised by ACES </h2>
+            <p>Date: 21 Sept ,2023</p>
+          </div>
         </div>
-    </div>);
+    </div>
+  </div>
+    </>
+  );
 }
 export default About;
--- src/Components/project/Project.jsx
+++ src/Components/project/Project.jsx
@@ -1,60 +1,112 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import ecom from './../images/ecom.png'
 import weatherImg from './../images/weather.png'
 import journalistImg from './../images/journalist.png'
 import expenseimg from './../images/expense.png'
 import healthImg from './../images/healthcoach.png'
+import studentImg from "./../images/student.png"
 import './project.css'
 function Project() {
-    const projects = [
-        {
-          id: 1,
-          title: "E-Commerce",
-          description: "Latest Project",
-          image: ecom
-          ,link:"https://e-commerce-caip-iy1eb02xy-amanroy7632s-projects.vercel.app/"
-        },
-        {
-          id: 2,
-          title: "Weather App",
-          description: "Description of Project 2",
-          image: weatherImg
-          ,link:"https://my-wather-app-ten.vercel.app/"
-        },
-        {
-          id: 3,
-          title: "Journalist Website",
-          description: "A full flesh frontend website for a ex-journalist which started teaching using this website.",
-          image: journalistImg
-          ,link:"https://journalist-react-app.vercel.app/"
-        },
-        {
-            id: 4,
-            title: "Expense Tracker",
-            description: "A user friendly web based expense tracker application which provides the ability to manage expense tracking for customers.",
-            image: expenseimg
-            ,link:"https://amanroy7632.github.io/CodeAlpha-Intern-project/ExpenseTrackerApp/"
-          },
-          {
-            id:5,
-            title:"Health Coach App",
-            description:"An Application where a health coach guides you to eat healthy foods and stay fit and wealthy in your life.",
-            image:healthImg,
-            link:"https://health-coach-qlx3i271f-amanroy7632s-projects.vercel.app/"
-          }
-      ];
+  const projects = [
+    {
+      id: 1,
+      title: "E-Commerce",
+      description: "Latest Project",
+      image: ecom
+      ,link:"https://e-commerce-caip-iy1eb02xy-amanroy7632s-projects.vercel.app/",
+      techUsed:"JavaScript React js , Tailwind CSS",
+      isNew:true
+    },
+    {
+      id: 2,
+      title: "Weather App",
+      description: "Description of Project 2",
+      image: weatherImg
+      ,link:"https://my-wather-app-ten.vercel.app/",
+      techUsed:"React js Tailwind CSS",
+      isNew:true
+    },
+    {
+      id: 3,
+      title: "Journalist Website",
+      description: "A full flesh frontend website for a ex-journalist which started teaching using this website.",
+      image: journalistImg
+      ,link:"https://journalist-react-app.vercel.app/",
+      techUsed:"React js , Tailwind CSS",
+      isNew:true
+    },
+    {
+        id: 4,
+        title: "Expense Tracker",
+        description: "A user friendly web based expense tracker application which provides the ability to manage expense tracking for customers.",
+        image: expenseimg
+        ,link:"https://amanroy7632.github.io/CodeAlpha-Intern-project/ExpenseTrackerApp/",
+        techUsed:"HTML, CSS, JavaScript",
+        isNew:false
+      },
+      {
+        id:5,
+        title:"Health Coach App",
+        description:"An Application where a health coach guides you to eat healthy foods and stay fit and wealthy in your life.",
+        image:healthImg,
+        link:"https://health-coach-qlx3i271f-amanroy7632s-projects.vercel.app/",
+        techUsed:"HTML, CSS, JavaScript",
+        isNew:false
+      },
+      {
+        id:6,
+        title:"Student Database Management",
+        description:"An Application where a student can login and access their results assignments and see their attendence . It is managed by an administrator of the College.",
+        image:studentImg,
+        link:"https://github.com/Amanroy7632/Student-Database-System/",
+        techUsed:"C++",
+        isNew:false
+      }
+  ];
+  const [name,setName]=useState("")
+  const [data,setData]=useState([])
+  useEffect(()=>{
+    // console.log(name);
+    if(!name){
+      setData(projects)
+    }else if(name==="latest"){
+
+      setData(projects.filter((item)=>item.isNew))
+    }
+     else{
+       setData(projects.filter((item)=>item.techUsed.includes(name)))
+
+     }
+  //  const setRef= setTimeout(() => {  
+  //     }, 1000);
+  //     return ()=>{
+  //       clearTimeout(setRef)
+  //     }
+  },[name])
+  const handleChange=(e)=>{
+    setName(e.target.value)
+  }
+    
   return (
     <div className="projects-container">
     <h1 className='heading mb-10 font-semibold'><span className='span-color'>My </span>Projects</h1>
+    <select name="" id="" className=' relative pb-2 rounded-l-md top-0 left-0 bg-transparent dark:bg-[#323946]' onChange={handleChange}>
+      <option value="">Filter out</option>
+      <option value="latest">Latest</option>
+      <option value="React">React js</option>
+      <option value="HTML">HTML,CSS</option>
+      <option value="C++">C++</option>
+    </select>
     <div className="projects">
-      {projects.map(project => (
+      {data.map(project => (
         <div className="project dark:bg-[#323946]" key={project.id}>
           {/* <img src={require(`${project.image}`).default}  alt={project.title} /> */}
        <Link to={project.link}> <img src={project.image}  alt={project.title} /> </Link>  
           <div className="project-info">
             <h2 className='title'>{project.title}</h2>
             <p>{project.description}</p>
+            <p>Tech Used: {project.techUsed}</p>
           </div>
         </div>
       ))}
